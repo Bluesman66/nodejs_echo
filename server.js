ca34ef2afe8cff16f155bf1258d24de639ea2dd1
alex.paclin <alex.paclin@gmail.com>
2017-10-15 23:01:31 +0500

    headers

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var http = require('http');
 var url = require('url');
 
 var server = new http.Server(function (req, res) {    
-    //console.log(req.method, req.url);
-    var urlParsed = url.parse(req.url, true);
-    //console.log(urlParsed);
+    console.log(req.headers);
 
+    var urlParsed = url.parse(req.url, true);
     if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.statusCode = 200;
+        res.setHeader('Cache-Control', 'no-cache');
         res.end(urlParsed.query.message);
     } else {
         res.statusCode = 404;
